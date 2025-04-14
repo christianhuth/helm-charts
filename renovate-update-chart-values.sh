89cfc8181c1ef6a8cb7446938dfb70818e6cc260
Christian Huth <christian@knell.it>
2025-04-14 16:22:13 +0200

    Update renovate-update-chart-values.sh

--- renovate-update-chart-values.sh
+++ renovate-update-chart-values.sh
@@ -3,51 +3,68 @@
 set -o pipefail
 
 [[ "${#}" == "4" ]] || {
-    echo "Please give the chart name, the update type, the old version and the new version of the dependency as only arguments"
+    echo "Please give the chart name, the update type, the old version and the new version of the Image as only arguments"
     exit 3
 }
 
 CHART="$1"
 UPDATE_TYPE="$2"
-DEP_VERSION_OLD="$3"
-DEP_VERSION_NEW="$4"
+IMAGE_VERSION_OLD="$3"
+IMAGE_VERSION_NEW="$4"
 
-echo "Old version is ${DEP_VERSION_OLD}"
-major="$(echo "${DEP_VERSION_OLD}" | cut -d. -f1)"
-minor="$(echo "${DEP_VERSION_OLD}" | cut -d. -f2)"
-patch="$(echo "${DEP_VERSION_OLD}" | cut -d. -f3)"
+CHART_VERSION_OLD="$(awk '/^version:/ {print $2}' "charts/${CHART}/Chart.yaml")"
+echo "Old versions are:"
+echo "Chart: ${CHART_VERSION_OLD}"
+echo "Image: ${IMAGE_VERSION_OLD}"
+
+CHART_VERSION_MAJOR="$(echo "${CHART_VERSION_OLD}" | cut -d. -f1)"
+CHART_VERSION_MINOR="$(echo "${CHART_VERSION_OLD}" | cut -d. -f2)"
+CHART_VERSION_PATCH="$(echo "${CHART_VERSION_OLD}" | cut -d. -f3)"
+
+IMAGE_VERSION_MAJOR="$(echo "${IMAGE_VERSION_OLD}" | cut -d. -f1)"
+IMAGE_VERSION_MINOR="$(echo "${IMAGE_VERSION_OLD}" | cut -d. -f2)"
+IMAGE_VERSION_PATCH="$(echo "${IMAGE_VERSION_OLD}" | cut -d. -f3)"
 
 if [[ "${UPDATE_TYPE}" =~ (major|replacement) ]]
 then
-  major="$(( major + 1 ))"
-  minor=0
-  patch=0
+  CHART_VERSION_MAJOR="$(( CHART_VERSION_MAJOR + 1 ))"
+  CHART_VERSION_MINOR=0
+  CHART_VERSION_PATCH=0
+  IMAGE_VERSION_MAJOR="$(( IMAGE_VERSION_MAJOR + 1 ))"
+  IMAGE_VERSION_MINOR=0
+  IMAGE_VERSION_PATCH=0
 elif [[ "${UPDATE_TYPE}" =~ 'minor' ]]
 then
-  minor="$(( minor + 1 ))"
-  patch=0
+  CHART_VERSION_MINOR="$(( CHART_VERSION_MINOR + 1 ))"
+  CHART_VERSION_PATCH=0
+  IMAGE_VERSION_MINOR="$(( IMAGE_VERSION_MINOR + 1 ))"
+  IMAGE_VERSION_PATCH=0
 else
-  patch="$(( patch + 1))"
+  CHART_VERSION_PATCH="$(( CHART_VERSION_PATCH + 1))"
+  IMAGE_VERSION_PATCH="$(( IMAGE_VERSION_PATCH + 1))"
 fi
 
-NEW_VERSION="${major}.${minor}.${patch}"
-echo "New version is ${NEW_VERSION}"
+CHART_VERSION_NEW="${CHART_VERSION_MAJOR}.${CHART_VERSION_MINOR}.${CHART_VERSION_PATCH}"
+IMAGE_VERSION_NEW="${IMAGE_VERSION_MAJOR}.${IMAGE_VERSION_MINOR}.${IMAGE_VERSION_PATCH}"
+echo "New versions are:"
+echo "Chart: ${CHART_VERSION_NEW}"
+echo "Image: ${IMAGE_VERSION_NEW}"
 
 # change appVersion in Chart.yaml
-sed -i "s/^appVersion:.*/appVersion: ${NEW_VERSION}/g" "charts/${CHART}/Chart.yaml"
+sed -i "s/^appVersion:.*/appVersion: ${IMAGE_VERSION_NEW}/g" "charts/${CHART}/Chart.yaml"
 
 # replace changes annotation for artifacthub in Chart.yaml
-changes=$"- kind: changed\n  description: appVersion to ${DEP_VERSION_NEW}\n"
+changes=$"- kind: changed\n  description: appVersion to ${IMAGE_VERSION_NEW}\n"
 yq eval ".annotations.\"artifacthub.io/changes\" = \"${changes}\"" -i charts/${CHART}/Chart.yaml
 
 # change image tag in values.yaml
-sed -i "s/${DEP_VERSION_OLD}/${DEP_VERSION_NEW}/g" "charts/${CHART}/values.yaml"
+sed -i "s/${IMAGE_VERSION_OLD}/${IMAGE_VERSION_NEW}/g" "charts/${CHART}/values.yaml"
 
 # change image tag in values.schema.json
-sed -i "s/${DEP_VERSION_OLD}/${DEP_VERSION_NEW}/g" "charts/${CHART}/values.schema.json"
+sed -i "s/${IMAGE_VERSION_OLD}/${IMAGE_VERSION_NEW}/g" "charts/${CHART}/values.schema.json"
 
 # change image tag in README.md
-sed -i "s/${DEP_VERSION_OLD}/${DEP_VERSION_NEW}/g" "charts/${CHART}/README.md"
+sed -i "s/${IMAGE_VERSION_OLD}/${IMAGE_VERSION_NEW}/g" "charts/${CHART}/README.md"
 
 # update CHANGELOG.md
 .github/generate-chart-changelogs.sh
