e2a1e088145d87878fb57ad7aa2247e7fadd41cb
Christian Huth <christian@knell.it>
2025-04-14 12:24:44 +0200

    Update renovate-action-entrypoint.sh

--- renovate-action-entrypoint.sh
+++ renovate-action-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-apt update
-
-apt install -y yq
-
-yq --version
+# Install yq
+YQ_VERSION="v4.43.1"
+wget https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64 -O /usr/local/bin/yq
+chmod +x /usr/local/bin/yq
+yq --version  # sanity check
 
 runuser -u ubuntu renovate
