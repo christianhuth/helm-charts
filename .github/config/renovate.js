a38756a9c1125565fe79e9393d5648650fbc3da4
Christian Huth <christian@knell.it>
2025-04-14 10:04:37 +0200

    Update renovate.js

--- .github/config/renovate.js
+++ .github/config/renovate.js
@@ -2,6 +2,6 @@ module.exports = {
   "platform": "github",
   "gitAuthor": "Renovate Bot <renovate@christianhuth.de>",
   "onboarding": false,
-  "allowPostUpgradeCommandTemplating": true,
+  "allowCommandTemplating": true,
   "allowedCommands": ["./renovate-update-chart-dependency.sh", "./renovate-update-chart-values.sh"]
 };
