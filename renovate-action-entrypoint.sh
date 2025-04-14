0401596afef9f8b18f4d4e9cfd23f74cbbbd08b3
Christian Huth <christian@knell.it>
2025-04-14 14:38:07 +0200

    Update renovate-action-entrypoint.sh

--- renovate-action-entrypoint.sh
+++ renovate-action-entrypoint.sh
@@ -5,8 +5,8 @@ OS=linux
 ARCH=amd64
 
 # Install gomplate
-#GOMPLATE_VERSION="v4.3.1"
-#curl -o /usr/local/bin/gomplate -sSL https://github.com/hairyhenderson/gomplate/releases/download/${GOMPLATE_VERSION}/gomplate_${OS}-${ARCH}
-#chmod 755 /usr/local/bin/gomplate
+GOMPLATE_VERSION="v4.3.0"
+curl -o /usr/local/bin/gomplate -sSL https://github.com/hairyhenderson/gomplate/releases/download/${GOMPLATE_VERSION}/gomplate_${OS}-${ARCH}
+chmod 755 /usr/local/bin/gomplate
 
 runuser -u ubuntu renovate
